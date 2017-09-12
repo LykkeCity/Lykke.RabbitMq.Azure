3083951028f86e1c295525fd144dcba2b7197702
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-09-12 20:45:46 +0300

    Exchange name now passed as Save/Load parameter

--- src/Lykke.RabbitMq.Azure/BlobPublishingQueueRepository.cs
+++ src/Lykke.RabbitMq.Azure/BlobPublishingQueueRepository.cs
@@ -5,31 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using AzureStorage;
 using Lykke.RabbitMqBroker.Publisher;
-using Lykke.RabbitMqBroker.Subscriber;
 using Newtonsoft.Json;
 
 namespace Lykke.RabbitMq.Azure
 {
-    public class BlobPublishingQueueRepository<TMessageModel, TMessageAbstraction> : IPublishingQueueRepository<TMessageAbstraction>
-        where TMessageModel : TMessageAbstraction, new()
+    public class BlobPublishingQueueRepository<TMessageImpl, TMessageAbstraction> : IPublishingQueueRepository<TMessageAbstraction>
+        where TMessageImpl : TMessageAbstraction, new()
     {
         private const string Container = "RabbitMqPublisherMessages";
         
         private readonly IBlobStorage _storage;
-        private readonly RabbitMqSubscriptionSettings _settings;
         private readonly string _instanceName;
 
         /// <param name="storage"></param>
-        /// <param name="settings"></param>
         /// <param name="instanceName">Instance name, required when multiple publishers publishes to single exchange</param>
-        public BlobPublishingQueueRepository(IBlobStorage storage, RabbitMqSubscriptionSettings settings, string instanceName = null)
+        public BlobPublishingQueueRepository(IBlobStorage storage, string instanceName)
         {
             _storage = storage;
-            _settings = settings;
             _instanceName = instanceName;
         }
 
-        public async Task SaveAsync(IReadOnlyCollection<TMessageAbstraction> items)
+        public async Task SaveAsync(IReadOnlyCollection<TMessageAbstraction> items, string exchangeName)
         {
             using (var stream = new MemoryStream())
             using (var streamWriter = new StreamWriter(stream, Encoding.UTF8))
@@ -45,18 +41,18 @@ namespace Lykke.RabbitMq.Azure
 
                 stream.Seek(0, SeekOrigin.Begin);
 
-                await _storage.SaveBlobAsync(Container, GetKey(), stream);
+                await _storage.SaveBlobAsync(Container, GetKey(exchangeName), stream);
             }
         }
 
-        public async Task<IReadOnlyCollection<TMessageAbstraction>> LoadAsync()
+        public async Task<IReadOnlyCollection<TMessageAbstraction>> LoadAsync(string exchangeName)
         {
-            if (!await _storage.HasBlobAsync(Container, GetKey()))
+            if (!await _storage.HasBlobAsync(Container, GetKey(exchangeName)))
             {
                 return null;
             }
 
-            using (var stream = await _storage.GetAsync(Container, GetKey()))
+            using (var stream = await _storage.GetAsync(Container, GetKey(exchangeName)))
             using (var streamReader = new StreamReader(stream, Encoding.UTF8))
             using (var jsonReader = new JsonTextReader(streamReader))
             {
@@ -66,17 +62,17 @@ namespace Lykke.RabbitMq.Azure
 
                 var serializer = new JsonSerializer();
 
-                return serializer.Deserialize<TMessageModel[]>(jsonReader)
+                return serializer.Deserialize<TMessageImpl[]>(jsonReader)
                     .Cast<TMessageAbstraction>()
                     .ToArray();
             }
         }
 
-        private string GetKey()
+        private string GetKey(string exchangeName)
         {
             return _instanceName != null
-                ? $"{_settings.ExchangeName}.{_instanceName}"
-                : _settings.ExchangeName;
+                ? $"{exchangeName}.{_instanceName}"
+                : exchangeName;
         }
     }
 }
