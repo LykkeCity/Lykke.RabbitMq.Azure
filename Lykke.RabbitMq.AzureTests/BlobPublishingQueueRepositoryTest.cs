b62165c7b95c5012073a15a79b64e53c84d54d42
Mikhail Gerasimov <mikhail.gerasimov@lykke.com>
2017-10-03 17:44:05 +0200

    Save messages from the queue as an array of bytes. (#1)
    * Save messages from the queue as an array of bytes.
    
    * Use InMemoryBlobStorage in tests. Update editorconfig.

---
+++ Lykke.RabbitMq.AzureTests/BlobPublishingQueueRepositoryTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using AzureStorage.Blob;
+using Lykke.RabbitMq.Azure;
+using NUnit.Framework;
+
+namespace Lykke.RabbitMq.AzureTests
+{
+    [TestFixture, Explicit("Depends on local azure storage emulator")]
+    internal sealed class BlobPublishingQueueRepositoryTest
+    {
+        private AzureBlobInMemory _azureBlobStorage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _azureBlobStorage = new AzureBlobInMemory();
+        }
+
+        [Test]
+        public async Task ShouldSerialize()
+        {
+            var repo = new BlobPublishingQueueRepository(_azureBlobStorage, "InstanceName");
+
+            var expected = new[] { new byte[] { 1, 2, 3 }, new byte[] { 4, 5, 6 } };
+
+            const string exchangeName = "ExchangeName";
+
+            await repo.SaveAsync(expected, exchangeName);
+            var actual = await repo.LoadAsync(exchangeName);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+    }
+}
--- src/Lykke.RabbitMq.Azure/BlobPublishingQueueRepository.cs
+++ src/Lykke.RabbitMq.Azure/BlobPublishingQueueRepository.cs
@@ -1,19 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading.Tasks;
 using AzureStorage;
 using Lykke.RabbitMqBroker.Publisher;
-using Newtonsoft.Json;
 
 namespace Lykke.RabbitMq.Azure
 {
-    public class BlobPublishingQueueRepository<TMessageImpl, TMessageAbstraction> : IPublishingQueueRepository<TMessageAbstraction>
-        where TMessageImpl : TMessageAbstraction, new()
+    public sealed class BlobPublishingQueueRepository : IPublishingQueueRepository
     {
         private const string Container = "RabbitMqPublisherMessages";
-        
+
         private readonly IBlobStorage _storage;
         private readonly string _instanceName;
 
@@ -25,46 +23,35 @@ namespace Lykke.RabbitMq.Azure
             _instanceName = instanceName;
         }
 
-        public async Task SaveAsync(IReadOnlyCollection<TMessageAbstraction> items, string exchangeName)
+        /// <summary>
+        /// Saves messages to azure storage
+        /// </summary>
+        /// <param name="items">Serialized messages</param>
+        /// <param name="exchangeName">Exchange name</param>
+        /// <returns></returns>
+        public async Task SaveAsync(IReadOnlyCollection<byte[]> items, string exchangeName)
         {
             using (var stream = new MemoryStream())
-            using (var streamWriter = new StreamWriter(stream, Encoding.UTF8))
-            using (var jsonWriter = new JsonTextWriter(streamWriter))
             {
-                var serializer = new JsonSerializer();
-                
-                serializer.Serialize(jsonWriter, items);
-
-                await jsonWriter.FlushAsync();
-                await streamWriter.FlushAsync();
-                await stream.FlushAsync();
-
-                stream.Seek(0, SeekOrigin.Begin);
-
+                var formater = new BinaryFormatter();
+                formater.Serialize(stream, items.ToArray());
                 await _storage.SaveBlobAsync(Container, GetKey(exchangeName), stream);
             }
         }
 
-        public async Task<IReadOnlyCollection<TMessageAbstraction>> LoadAsync(string exchangeName)
+
+        public async Task<IReadOnlyCollection<byte[]>> LoadAsync(string exchangeName)
         {
             if (!await _storage.HasBlobAsync(Container, GetKey(exchangeName)))
             {
-                return null;
+                return new byte[0][];
             }
 
             using (var stream = await _storage.GetAsync(Container, GetKey(exchangeName)))
-            using (var streamReader = new StreamReader(stream, Encoding.UTF8))
-            using (var jsonReader = new JsonTextReader(streamReader))
             {
-                await stream.FlushAsync();
-
-                stream.Seek(0, SeekOrigin.Begin);
-
-                var serializer = new JsonSerializer();
-
-                return serializer.Deserialize<TMessageImpl[]>(jsonReader)
-                    .Cast<TMessageAbstraction>()
-                    .ToArray();
+                var formater = new BinaryFormatter();
+                var result = (IReadOnlyCollection<byte[]>)(byte[][])formater.Deserialize(stream);
+                return result;
             }
         }
 
