11b6eb6cf2f06f62b50dede76f0f1d8b4c5f5703
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-10-07 01:38:05 +0300

    - MessagePack repository was added - Migration from v 2.x format to MessagePack format  was added

--- Lykke.RabbitMq.AzureTests/MessagePackBlobPublishingQueueRepositoryTest.cs
+++ Lykke.RabbitMq.AzureTests/MessagePackBlobPublishingQueueRepositoryTest.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 namespace Lykke.RabbitMq.AzureTests
 {
     [TestFixture]
-    internal sealed class BlobPublishingQueueRepositoryTest
+    internal sealed class MessagePackBlobPublishingQueueRepositoryTest
     {
         private AzureBlobInMemory _azureBlobStorage;
 
@@ -17,17 +17,21 @@ namespace Lykke.RabbitMq.AzureTests
         }
 
         [Test]
-        public async Task ShouldSerialize()
+        public async Task ShouldDeserializeSerialized()
         {
-            var repo = new BlobPublishingQueueRepository(_azureBlobStorage, "InstanceName");
+            // Arrange
+            var repo = new MessagePackBlobPublishingQueueRepository(_azureBlobStorage, "InstanceName");
 
             var expected = new[] { new byte[] { 1, 2, 3 }, new byte[] { 4, 5, 6 } };
 
             const string exchangeName = "ExchangeName";
 
             await repo.SaveAsync(expected, exchangeName);
+            
+            // Act
             var actual = await repo.LoadAsync(exchangeName);
 
+            // Assert
             Assert.That(expected, Is.EqualTo(actual));
         }
     }
---
+++ src/Lykke.RabbitMq.Azure/V2BlobPublishingQueueRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using AzureStorage;
+using Newtonsoft.Json;
+
+namespace Lykke.RabbitMq.Azure
+{
+    [Obsolete("Used only for data migration")]
+    internal class V2BlobPublishingQueueRepository<TMessage>
+    {
+        private const string Container = "RabbitMqPublisherMessages";
+
+        private readonly IBlobStorage _storage;
+        private readonly string _instanceName;
+
+        /// <param name="storage"></param>
+        /// <param name="instanceName">Instance name, required when multiple publishers publishes to single exchange</param>
+        public V2BlobPublishingQueueRepository(IBlobStorage storage, string instanceName = null)
+        {
+            _storage = storage;
+            _instanceName = instanceName;
+        }
+
+        public async Task SaveAsync(IReadOnlyCollection<TMessage> items, string exchangeName)
+        {
+            using (var stream = new MemoryStream())
+            using (var streamWriter = new StreamWriter(stream, Encoding.UTF8))
+            using (var jsonWriter = new JsonTextWriter(streamWriter))
+            {
+                var serializer = new JsonSerializer();
+
+                serializer.Serialize(jsonWriter, items);
+
+                await jsonWriter.FlushAsync();
+                await streamWriter.FlushAsync();
+                await stream.FlushAsync();
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                await _storage.SaveBlobAsync(Container, GetKey(exchangeName), stream);
+            }
+        }
+
+        public async Task<IReadOnlyCollection<TMessage>> LoadAsync(string exchangeName)
+        {
+            if (!await _storage.HasBlobAsync(Container, GetKey(exchangeName)))
+            {
+                return null;
+            }
+
+            using (var stream = await _storage.GetAsync(Container, GetKey(exchangeName)))
+            using (var streamReader = new StreamReader(stream, Encoding.UTF8))
+            using (var jsonReader = new JsonTextReader(streamReader))
+            {
+                await stream.FlushAsync();
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var serializer = new JsonSerializer();
+
+                return serializer.Deserialize<TMessage[]>(jsonReader);
+            }
+        }
+
+        private string GetKey(string exchangeName)
+        {
+            return _instanceName != null
+                ? $"{exchangeName}.{_instanceName}"
+                : exchangeName;
+        }
+    }
+}
