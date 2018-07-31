b117c67343eb3955d0fba4a31a1cc278ade45a87
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-07-31 15:54:13 +0300

    added azure storage deduplicator

---
+++ src/Lykke.RabbitMq.Azure/Deduplicator/AzureStorageDeduplicator.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using AzureStorage;
+using AzureStorage.Tables;
+using Lykke.Common.Log;
+using Lykke.RabbitMqBroker.Deduplication;
+using Lykke.SettingsReader.ReloadingManager;
+
+namespace Lykke.RabbitMq.Azure.Deduplicator
+{
+    /// <inheritdoc />
+    /// <remarks>Azure table storage implementation</remarks>
+    public class AzureStorageDeduplicator : IDeduplicator
+    {
+        private readonly DuplicatesRepository _repository;
+
+        public AzureStorageDeduplicator(INoSQLTableStorage<DuplicateEntity> tableStorage)
+        {
+            _repository = new DuplicatesRepository(tableStorage);
+        }
+
+        public static AzureStorageDeduplicator Create(string connString, string tableName, ILogFactory log)
+        {
+            return new AzureStorageDeduplicator(AzureTableStorage<DuplicateEntity>.Create(
+                ConstantReloadingManager.From(connString), tableName, log));
+        }
+        
+        public async Task<bool> EnsureNotDuplicateAsync(byte[] value)
+        {
+            return await _repository.EnsureNotDuplicateAsync(value);
+        }
+    }
+}
---
+++ tests/Lykke.RabbitMq.Azure.Tests/AzureStorageDeduplcatorTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using AzureStorage.Tables;
+using Lykke.RabbitMq.Azure.Deduplicator;
+using NUnit.Framework;
+
+namespace Lykke.RabbitMq.Azure.Tests
+{
+    [TestFixture]
+    public class AzureStorageDeduplcatorTest
+    {
+        private readonly AzureStorageDeduplicator _deduplcator = new AzureStorageDeduplicator(new NoSqlTableInMemory<DuplicateEntity>());
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public async Task EnsureNotDuplicateAsync()
+        {
+            var value = new byte[] {1, 2, 3 };
+
+            var notDuplicate = await _deduplcator.EnsureNotDuplicateAsync(value);
+            Assert.True(notDuplicate);
+
+            notDuplicate = await _deduplcator.EnsureNotDuplicateAsync(value);
+            Assert.False(notDuplicate);
+        }
+    }
+}
